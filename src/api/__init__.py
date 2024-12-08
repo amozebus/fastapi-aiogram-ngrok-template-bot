a27224e682f04afe55d3684812af12d5c8b6d311
amozebus <amozebus@gmail.com>
2024-12-09 03:15:00 +0600

    added lifespan logging, hotfix: lifespan takes 1 argument (app)

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,5 @@
 """App initialization"""
+import logging
 
 from contextlib import asynccontextmanager
 
@@ -12,10 +13,12 @@ from .routes import r, WEBHOOK_PATH
 
 
 @asynccontextmanager
-async def lifespan():
+async def lifespan(app: FastAPI):
     """On-startup and on-shutdown events"""
+    logging.info(f"{app.title} startup")
     await bot.set_webhook(f"{settings.NGROK_URL}{WEBHOOK_PATH}")
     yield
+    logging.info(f"{app.title} shutdown")
     await bot.delete_webhook()
 
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 """Launch app with uvicorn web server"""
+import logging
 
 import uvicorn
 
 from api import app
 
+LOG_LEVEL = logging.INFO
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    logging.basicConfig(level=LOG_LEVEL)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level=LOG_LEVEL)
